6d298320f63c7f513db46e5fc418f63f78b0c2b3
Priyanshu Dhall <priyanshudhall2001@gmail.com>
2024-10-21 19:23:15 +0530

    [routing]: basics

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Hi I'm Priyanshu, and with this project I'm trying to learn Next.js.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>
+        Hi I'm Priyanshu, software developer based in India. Also known as
+        anorangefalcon on GitHub.
+      </p>
+    </div>
+  );
+}
