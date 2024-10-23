64ed3779acc6807c069f5f0a25149611b95d53a3
Priyanshu Dhall <priyanshudhall2001@gmail.com>
2024-10-23 19:56:23 +0530

    [routing]: route groups (folderName)

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  // these routes are logically grouped in auth folder but can directly be accessed by /login without need of /auth/login
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  // these routes are logically grouped in auth folder but can directly be accessed by /register without need of /auth/register
+
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+}
